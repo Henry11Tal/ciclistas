798ff3c89282219dc974f5474c143a8d85ed5172
Henry11Tal <htalavera1@educa.madrid.org>
2026-02-09 13:34:56 +0100

    Modificar las Rutas de API

--- routes/api.php
+++ routes/api.php
@@ -24,28 +24,28 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/api/register', [AutenticacionController::class, 'registerAPI'])->name('autenticacion.registerAPI');
-Route::post('/api/login', [AutenticacionController::class, 'loginAPI'])->name('autenticacion.loginAPI');
-Route::post('/api/logout', [AutenticacionController::class, 'logoutAPI'])->name('autenticacion.logoutAPI');
+Route::post('/register', [AutenticacionController::class, 'registerAPI'])->name('autenticacion.registerAPI');
+Route::post('/login', [AutenticacionController::class, 'loginAPI'])->name('autenticacion.loginAPI');
+Route::post('/logout', [AutenticacionController::class, 'logoutAPI'])->name('autenticacion.logoutAPI');
 
-Route::get('/api/bloque', [BloqueController::class, 'listarBloquesAPI'])->name('bloque.listarBloquesAPI');
-Route::post('/api/bloque/crear', [BloqueController::class, 'crearBloqueAPI'])->name('bloque.crearBloqueAPI');
-Route::get('/api/bloque/{id}', [BloqueController::class, 'obtenerBloqueAPI'])->name('bloque.obtenerBloqueAPI');
-Route::delete('/api/bloque/{id}', [BloqueController::class, 'borrarBloqueAPI'])->name('bloque.borrarBloqueAPI');
+Route::get('/bloque', [BloqueController::class, 'listarBloquesAPI'])->name('bloque.listarBloquesAPI');
+Route::post('/bloque/crear', [BloqueController::class, 'crearBloqueAPI'])->name('bloque.crearBloqueAPI');
+Route::get('/bloque/{id}', [BloqueController::class, 'obtenerBloqueAPI'])->name('bloque.obtenerBloqueAPI');
+Route::delete('/bloque/{id}', [BloqueController::class, 'borrarBloqueAPI'])->name('bloque.borrarBloqueAPI');
 
-Route::get('/api/plan', [PlanController::class, 'listarPlanesAPI'])->name('plan.listarPlanesAPI');
-Route::post('/api/plan/crear', [PlanController::class, 'crearPlanAPI'])->name('plan.crearPlanAPI');
-Route::put('/api/plan/{id}', [PlanController::class, 'modificarPlanAPI'])->name('plan.modificarPlanAPI');
-Route::delete('/api/plan/{id}', [PlanController::class, 'borrarPlanAPI'])->name('plan.borrarPlanAPI');
+Route::get('/plan', [PlanController::class, 'listarPlanesAPI'])->name('plan.listarPlanesAPI');
+Route::post('/plan/crear', [PlanController::class, 'crearPlanAPI'])->name('plan.crearPlanAPI');
+Route::put('/plan/{id}', [PlanController::class, 'modificarPlanAPI'])->name('plan.modificarPlanAPI');
+Route::delete('/plan/{id}', [PlanController::class, 'borrarPlanAPI'])->name('plan.borrarPlanAPI');
 
-Route::get('/api/sesion', [SesionController::class, 'listarSesionesAPI'])->name('sesion.listarSesionesAPI');
-Route::post('/api/sesion/crear', [SesionController::class, 'crearSesionAPI'])->name('sesion.crearSesionAPI');
-Route::get('/api/sesion/{id}', [SesionController::class, 'obtenerSesionAPI'])->name('sesion.obtenerSesionAPI');
-Route::delete('/api/sesion/{id}', [SesionController::class, 'borrarSesionAPI'])->name('sesion.borrarSesionAPI');
+Route::get('/sesion', [SesionController::class, 'listarSesionesAPI'])->name('sesion.listarSesionesAPI');
+Route::post('/sesion/crear', [SesionController::class, 'crearSesionAPI'])->name('sesion.crearSesionAPI');
+Route::get('/sesion/{id}', [SesionController::class, 'obtenerSesionAPI'])->name('sesion.obtenerSesionAPI');
+Route::delete('/sesion/{id}', [SesionController::class, 'borrarSesionAPI'])->name('sesion.borrarSesionAPI');
 
-Route::post('/api/resultado/crear', [ResultadoController::class, 'crearResultadoAPI'])->name('resultado.crearResultadoAPI');
-Route::get('/api/resultado/{id}', [ResultadoController::class, 'obtenerResultadoAPI'])->name('resultado.obtenerResultadoAPI');
+Route::post('/resultado/crear', [ResultadoController::class, 'crearResultadoAPI'])->name('resultado.crearResultadoAPI');
+Route::get('/resultado/{id}', [ResultadoController::class, 'obtenerResultadoAPI'])->name('resultado.obtenerResultadoAPI');
 
-Route::get('/api/sesionbloque', [SesionBloqueController::class, 'listarSesionBloqueAPI'])->name('sesionbloque.listarSesionBloqueAPI');
-Route::post('/api/sesionbloque/crear', [SesionBloqueController::class, 'crearSesionBloqueAPI'])->name('sesionbloque.crearSesionBloqueAPI');
-Route::delete('/api/sesionbloque/{id}', [SesionBloqueController::class, 'borrarSesionBloqueAPI'])->name('sesionbloque.borrarSesionBloqueAPI');
+Route::get('/sesionbloque', [SesionBloqueController::class, 'listarSesionBloqueAPI'])->name('sesionbloque.listarSesionBloqueAPI');
+Route::post('/sesionbloque/crear', [SesionBloqueController::class, 'crearSesionBloqueAPI'])->name('sesionbloque.crearSesionBloqueAPI');
+Route::delete('/sesionbloque/{id}', [SesionBloqueController::class, 'borrarSesionBloqueAPI'])->name('sesionbloque.borrarSesionBloqueAPI');
